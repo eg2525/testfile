2dbd9612b368793852a8ce4e047e060e13d94a52
eg2525 <149653970+eg2525@users.noreply.github.com>
2023-11-16 16:33:31 +0900

    Add files via upload

--- 仕訳インポート.py
+++ 仕訳インポート.py
@@ -26,12 +26,8 @@ def app1():
             elif uploaded_file.name.endswith('.xlsx'):
                 # Excelファイルを読み込む
                 df = pd.read_excel(uploaded_file)
-            
-            # データフレームを表示
-            st.dataframe(df)
 
             # 新しいDataFrameを作成
-            # （例: "日付", "伝票番号", ... などのヘッダーを追加）
             new_headers = ["日付","伝票番号","決算整理仕訳","借方勘定科目","借方科目コード","借方補助科目","借方取引先","借方取引先コード","借方部門","借方品目","借方メモタグ","借方セグメント1","借方セグメント2","借方セグメント3","借方金額","借方税区分","借方税額","貸方勘定科目","貸方科目コード","貸方補助科目","貸方取引先","貸方取引先コード","貸方部門","貸方品目","貸方メモタグ","貸方セグメント1","貸方セグメント2","貸方セグメント3","貸方金額","貸方税区分","貸方税額","摘要"]  # ヘッダーのリストを続ける
             new_df = pd.DataFrame(columns=new_headers)
             # ここで新しいDataFrameにデータを追加・編集する処理があればここに記述
@@ -40,14 +36,23 @@ def app1():
             st.error(f"エラーが発生しました: {e}")
             # ここにエラー処理のコードを追加する
 
-    # ダウンロードリンクの生成と提供
+    # Excelファイルとしてユーザーにダウンロードさせる関数
+    def convert_df_to_excel(df):
+        output = BytesIO()
+        writer = pd.ExcelWriter(output, engine='openpyxl')
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+        writer.save()
+        processed_data = output.getvalue()
+        return processed_data
+
+    # ダウンロードボタンを表示する
     if 'new_df' in locals():
-        towrite = BytesIO()
-        new_df.to_excel(towrite, index=False, header=True, engine='openpyxl')
-        towrite.seek(0)  # ファイルポインタを先頭に戻す
-        b64 = base64.b64encode(towrite.read()).decode()  # バイトデータをbase64にエンコード
-        href = f'<a href="data:application/octet-stream;base64,{b64}" download="新規データ.xlsx">新規データ.xlsxをダウンロード</a>'
-        st.markdown(href, unsafe_allow_html=True)
+        st.download_button(
+            label="新しいExcelファイルをダウンロード",
+            data=convert_df_to_excel(new_df),
+            file_name='新規データ.xlsx',
+            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
 
 # アプリを実行
 app1()
