8f3d589a9481a3047fb1ec54631893d63e48db0f
eg2525 <149653970+eg2525@users.noreply.github.com>
2023-11-16 17:29:57 +0900

    Add files via upload

--- 仕訳インポート.py
+++ 仕訳インポート.py
@@ -16,18 +16,21 @@ def app1():
             df = pd.read_excel(uploaded_file)
 
         uploaded_headers = df.columns.tolist()
-        output_headers = ["日付","伝票番号","決算整理仕訳","借方勘定科目","借方科目コード","借方補助科目","借方取引先","借方取引先コード","借方部門","借方品目","借方メモタグ","借方セグメント1","借方セグメント2","借方セグメント3","借方金額","借方税区分","借方税額","貸方勘定科目","貸方科目コード","貸方補助科目","貸方取引先","貸方取引先コード","貸方部門","貸方品目","貸方メモタグ","貸方セグメント1","貸方セグメント2","貸方セグメント3","貸方金額","貸方税区分","貸方税額","摘要"]  # 出力ファイルのヘッダーリスト
-        output_headers_with_option = ["転記しない"] + output_headers  # プルダウン用に「転記しない」オプションを追加
+        output_headers = ["日付", "伝票番号", "..."]  # ここには実際の出力ヘッダーをリストとして追加します
+        output_headers_with_option = ["転記しない"] + output_headers
 
+        # ヘッダーとプルダウンを横に並べて表示
         mappings = {}
         for header in uploaded_headers:
             col1, col2 = st.columns([3, 2])  # 2列レイアウトを作成
             with col1:
                 st.text(header)
             with col2:
-                mappings[header] = st.selectbox("", output_headers_with_option, key=header)
+                # デフォルトで「転記しない」が選択されているようにする
+                mappings[header] = st.selectbox(f"{header} に対応するヘッダー:", options=output_headers_with_option, index=0)
 
         if st.button('OK'):
+            # マッピングに基づいてデータを新しいDataFrameに転記
             new_df = pd.DataFrame()
             for in_header, out_header in mappings.items():
                 if out_header != "転記しない":  # 「転記しない」が選択されていない場合に転記
