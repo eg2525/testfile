528410fce479288b36b0ec963a9b3b8a30d29af3
eg2525 <149653970+eg2525@users.noreply.github.com>
2023-11-17 17:03:32 +0900

    Add files via upload

--- 仕訳インポート.py
+++ 仕訳インポート.py
@@ -5,35 +5,30 @@ import base64
 
 def app1():
     st.title('仕訳インポート')
-    # ...[省略]...
+    st.markdown("ここにアプリの説明文を追加します。")
 
     uploaded_file = st.file_uploader("ファイルをアップロードしてください", type=['csv', 'xlsx'])
 
     if uploaded_file is not None:
-        # ...[ファイル読み込み処理省略]...
+        if uploaded_file.name.endswith('.csv'):
+            df = pd.read_csv(uploaded_file, encoding='cp932')
+        elif uploaded_file.name.endswith('.xlsx'):
+            df = pd.read_excel(uploaded_file)
 
-        # 固定されたヘッダーのリスト
-        fixed_headers = [
-            "日付", "伝票番号", "決算整理仕訳", "借方勘定科目", "借方科目コード", "借方補助科目",
-            "借方取引先", "借方取引先コード", "借方部門", "借方品目", "借方メモタグ",
-            "借方セグメント1", "借方セグメント2", "借方セグメント3", "借方金額", "借方税区分",
-            "借方税額", "貸方勘定科目", "貸方科目コード", "貸方補助科目", "貸方取引先",
-            "貸方取引先コード", "貸方部門", "貸方品目", "貸方メモタグ", "貸方セグメント1",
-            "貸方セグメント2", "貸方セグメント3", "貸方金額", "貸方税区分", "貸方税額", "摘要"
-        ]
-
-        # 初期化された新しいDataFrameを作成
-        new_df = pd.DataFrame(columns=fixed_headers)
-
-        # ...[マッピング処理省略]...
-
+        # 出力ファイルの固定されたヘッダー
+        fixed_headers = ["日付","伝票番号","決算整理仕訳","借方勘定科目","借方科目コード","借方補助科目","借方取引先","借方取引先コード","借方部門","借方品目","借方メモタグ","借方セグメント1","借方セグメント2","借方セグメント3","借方金額","借方税区分","借方税額","貸方勘定科目","貸方科目コード","貸方補助科目","貸方取引先","貸方取引先コード","貸方部門","貸方品目","貸方メモタグ","貸方セグメント1","貸方セグメント2","貸方セグメント3","貸方金額","貸方税区分","貸方税額","摘要"
+]  # 全部のヘッダーをここに追加
+        # ヘッダーのマッピングを設定するためのプルダウンメニューを生成
+        header_mapping = {fh: st.selectbox(f"{fh}:", [""] + list(df.columns), index=0) for fh in fixed_headers}
+        
         if st.button('OK'):
             # マッピングに基づいてデータを新しいDataFrameに転記
-            for fixed_header, selected_header in mappings.items():
-                if selected_header != "転記しない" and selected_header in df.columns:
-                    new_df[fixed_header] = df[selected_header]
+            new_df = pd.DataFrame(columns=fixed_headers)
+            for fh in fixed_headers:
+                if header_mapping[fh]:  # 空文字列でない場合にのみ転記
+                    new_df[fh] = df[header_mapping[fh]]
                 else:
-                    new_df[fixed_header] = pd.NA  # マッピングされない場合は欠損値を入れ
+                    new_df[fh] = ""  # 空文字列で初期化
 
             # Excelファイルのダウンロードリンクを提供
             towrite = BytesIO()
