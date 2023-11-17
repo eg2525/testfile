81562dd55a6030d06f1dd1115018d7a3e19c611a
eg2525 <149653970+eg2525@users.noreply.github.com>
2023-11-17 16:08:27 +0900

    Add files via upload

--- 仕訳インポート.py
+++ 仕訳インポート.py
@@ -5,40 +5,35 @@ import base64
 
 def app1():
     st.title('仕訳インポート')
-    st.markdown("ここにアプリの説明文を追加します。")
+    # ...[省略]...
 
     uploaded_file = st.file_uploader("ファイルをアップロードしてください", type=['csv', 'xlsx'])
 
     if uploaded_file is not None:
-        if uploaded_file.name.endswith('.csv'):
-            df = pd.read_csv(uploaded_file, encoding='cp932')
-        elif uploaded_file.name.endswith('.xlsx'):
-            df = pd.read_excel(uploaded_file)
+        # ...[ファイル読み込み処理省略]...
 
-        # 出力ファイルの固定されたヘッダー
-        fixed_headers = ["日付","伝票番号","決算整理仕訳","借方勘定科目","借方科目コード","借方補助科目","借方取引先","借方取引先コード","借方部門","借方品目","借方メモタグ","借方セグメント1","借方セグメント2","借方セグメント3","借方金額","借方税区分","借方税額","貸方勘定科目","貸方科目コード","貸方補助科目","貸方取引先","貸方取引先コード","貸方部門","貸方品目","貸方メモタグ","貸方セグメント1","貸方セグメント2","貸方セグメント3","貸方金額","貸方税区分","貸方税額","摘要"
-] 
+        # 固定されたヘッダーのリスト
+        fixed_headers = [
+            "日付", "伝票番号", "決算整理仕訳", "借方勘定科目", "借方科目コード", "借方補助科目",
+            "借方取引先", "借方取引先コード", "借方部門", "借方品目", "借方メモタグ",
+            "借方セグメント1", "借方セグメント2", "借方セグメント3", "借方金額", "借方税区分",
+            "借方税額", "貸方勘定科目", "貸方科目コード", "貸方補助科目", "貸方取引先",
+            "貸方取引先コード", "貸方部門", "貸方品目", "貸方メモタグ", "貸方セグメント1",
+            "貸方セグメント2", "貸方セグメント3", "貸方金額", "貸方税区分", "貸方税額", "摘要"
+        ]
 
-        # アップロードされたファイルのヘッダーを取得
-        uploaded_headers = df.columns.tolist()
-        # プルダウン用に「転記しない」オプションを追加
-        options = ["転記しない"] + uploaded_headers
+        # 初期化された新しいDataFrameを作成
+        new_df = pd.DataFrame(columns=fixed_headers)
 
-        mappings = {}
-        for fixed_header in fixed_headers:
-            col1, col2 = st.columns([3, 2])
-            with col1:
-                st.text(fixed_header)
-            with col2:
-                # 固定ヘッダーに対応するアップロードされたファイルのヘッダーを選択
-                mappings[fixed_header] = st.selectbox("", options, key=fixed_header)
+        # ...[マッピング処理省略]...
 
         if st.button('OK'):
             # マッピングに基づいてデータを新しいDataFrameに転記
-            new_df = pd.DataFrame(columns=fixed_headers)  # 出力ファイルのDataFrameを作成
             for fixed_header, selected_header in mappings.items():
-                if selected_header != "転記しない":
-                    new_df[fixed_header] = df[selected_header] if selected_header in df else None
+                if selected_header != "転記しない" and selected_header in df.columns:
+                    new_df[fixed_header] = df[selected_header]
+                else:
+                    new_df[fixed_header] = pd.NA  # マッピングされない場合は欠損値を入れ
 
             # Excelファイルのダウンロードリンクを提供
             towrite = BytesIO()
