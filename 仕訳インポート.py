87e16357a421c6626da8fbe29f7fb0d8898a165f
eg2525 <149653970+eg2525@users.noreply.github.com>
2023-11-20 10:55:15 +0900

    Add files via upload

--- 仕訳インポート.py
+++ 仕訳インポート.py
@@ -17,18 +17,28 @@ def app1():
 
         # 出力ファイルの固定されたヘッダー
         fixed_headers = ["日付","伝票番号","決算整理仕訳","借方勘定科目","借方科目コード","借方補助科目","借方取引先","借方取引先コード","借方部門","借方品目","借方メモタグ","借方セグメント1","借方セグメント2","借方セグメント3","借方金額","借方税区分","借方税額","貸方勘定科目","貸方科目コード","貸方補助科目","貸方取引先","貸方取引先コード","貸方部門","貸方品目","貸方メモタグ","貸方セグメント1","貸方セグメント2","貸方セグメント3","貸方金額","貸方税区分","貸方税額","摘要"
-]  # 全部のヘッダーをここに追加
-        # ヘッダーのマッピングを設定するためのプルダウンメニューを生成
-        header_mapping = {fh: st.selectbox(f"{fh}:", [""] + list(df.columns), index=0) for fh in fixed_headers}
-        
+] 
+
+        # アップロードされたファイルのヘッダーを取得
+        uploaded_headers = df.columns.tolist()
+        # プルダウン用に「転記しない」オプションを追加
+        options = ["転記しない"] + fixed_headers
+
+        mappings = {}
+        for fixed_header in fixed_headers:
+            col1, col2 = st.columns([3, 2])
+            with col1:
+                st.text(fixed_header)
+            with col2:
+                # 固定ヘッダーに対応するアップロードされたファイルのヘッダーを選択
+                mappings[fixed_header] = st.selectbox("", options, key=fixed_header)
+
         if st.button('OK'):
             # マッピングに基づいてデータを新しいDataFrameに転記
-            new_df = pd.DataFrame(columns=fixed_headers)
-            for fh in fixed_headers:
-                if header_mapping[fh]:  # 空文字列でない場合にのみ転記
-                    new_df[fh] = df[header_mapping[fh]]
-                else:
-                    new_df[fh] = ""  # 空文字列で初期化
+            new_df = pd.DataFrame(columns=fixed_headers)  # 出力ファイルのDataFrameを作成
+            for fixed_header, selected_header in mappings.items():
+                if selected_header != "転記しない":
+                    new_df[fixed_header] = df[selected_header] if selected_header in df else None
 
             # Excelファイルのダウンロードリンクを提供
             towrite = BytesIO()
