9219d1bca412304c173777af75f5be5757a1b826
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-04-17 16:45:16 +0900

    Add files via upload

--- data_import_bright.py
+++ data_import_bright.py
@@ -12,7 +12,7 @@ months = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月"
 # ユーザーから月を選択してもらう
 selected_month = st.selectbox("読み込むシート名を選択してください:", months)
 
-# チェックボックスで処理開始
+# 処理開始のチェックボックスがチェックされ、ファイルがアップロードされ、処理が完了した後にデータフレームを表示
 if st.checkbox('処理開始'):
     if uploaded_files and selected_month:
         dataframes = {}
@@ -23,7 +23,7 @@ if st.checkbox('処理開始'):
             dataframes[uploaded_file.name] = df
 
         columns = ["つくば", "羽生", "王子", "三鷹", "仙台", "川口", "船橋", "南森町", "高田馬場", "横浜関内", "福岡天神", "大宮"]
-        index = ["自費", "社保", "国保", "販売品", "過不足金", "保険返金", "その他/保険証忘れ", "売上合計", "窓口現金", "窓口クレジット", "窓口合計", "精算機現金", "精算機クレジット", "取消し", "精算機合計", "振込入金", "自費返金", "JACCS入金", "口座振替", "[点]後期高齢", "[点]国保窓口入金", "[点]社保窓口入金", "[点]合計", "[一・負]後期高齢", "[一・負]国保窓口入金", "[一・負]社保窓口入金", "[一・負]合計", "差額"]
+        index = ["自費", "社保", "国保", "販売品", "過不足金", "保険返金", "その他/保険証忘れ", "売上合計", "窓口現金", "窓口クレジット", "窓口合計", "精算機現金", "精算機クレジット", "取消し", "精算機合計", "振込入金", "自費返金", "JACCS入金", "口座振替"]
         
         final_df = pd.DataFrame(index=index, columns=columns)
         for filename, data in dataframes.items():
@@ -34,35 +34,6 @@ if st.checkbox('処理開始'):
                         if clean_index in data.columns:
                             value = data[clean_index].iloc[31] if len(data[clean_index]) > 31 else None
                             final_df.at[idx, column] = value
-        
-        edited_df = st.data_editor(final_df)
 
-        if st.button("変更を保存"):
-            output_columns = ['収支区分', '発生日', '取引先', '税区分', '勘定科目', '品目', '部門', '金額']
-            output_df = pd.DataFrame(columns=output_columns)
-
-            for col in final_df.columns:
-                for idx in final_df.index:
-                    value = final_df.at[idx, col]
-
-                    # 空文字やNaNをNoneに統一し、数値型に変換を試みる
-                    if pd.isna(value):
-                        value = None
-                    else:
-                        try:
-                            # 数値型への強制変換を試みる
-                            value = float(value)
-                        except ValueError:
-                            continue  # 数値に変換できない場合はスキップ
-
-                    # Noneまたは0ではない場合に転記
-                    if value is not None and value != 0:
-                        new_row = pd.DataFrame({
-                            '品目': [idx],
-                            '部門': [col],
-                            '金額': [value]
-                        }, columns=output_columns)
-                        output_df = pd.concat([output_df, new_row], ignore_index=True)
-
-            st.success('変更がfinal_dfに保存され、データがoutput_dfに転記されました。')
-            st.write(output_df)
+        # ここでデータフレームを表示します
+        st.dataframe(final_df)
