2329502ca158ffaa49328cd0c7f63d989a97ed3d
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-04-17 14:09:47 +0900

    Add files via upload

--- data_import_bright.py
+++ data_import_bright.py
@@ -22,7 +22,7 @@ if st.checkbox('処理開始'):
             df.columns = [col.replace('\n', '') for col in df.columns]
             dataframes[uploaded_file.name] = df
 
-        columns = ["つくば", "羽生", "王子", "三鷹", "仙台", "川口", "船橋", "南森町", "高田馬場", "関内", "福岡天神", "大宮"]
+        columns = ["つくば", "羽生", "王子", "三鷹", "仙台", "川口", "船橋", "南森町", "高田馬場", "横浜関内", "福岡天神", "大宮"]
         index = ["自費", "社保", "国保", "販売品", "過不足金", "保険返金", "その他/保険証忘れ", "売上合計", "窓口現金", "窓口クレジット", "窓口合計", "精算機現金", "精算機クレジット", "取消し", "精算機合計", "振込入金", "自費返金", "JACCS入金", "口座振替", "[点]後期高齢", "[点]国保窓口入金", "[点]社保窓口入金", "[点]合計", "[一・負]後期高齢", "[一・負]国保窓口入金", "[一・負]社保窓口入金", "[一・負]合計", "差額"]
         
         final_df = pd.DataFrame(index=index, columns=columns)
@@ -38,6 +38,6 @@ if st.checkbox('処理開始'):
         edited_df = st.data_editor(final_df)
 
         if st.button("変更を保存"):
-            csv_file_name = 'updated_data.csv'
-            edited_df.to_csv(csv_file_name, encoding='utf-8')
-            st.download_button("Download updated data as CSV", edited_df.to_csv().encode('cp932'), file_name=csv_file_name, mime='text/csv')
+            # edited_dfの内容をfinal_dfに上書き保存
+            final_df.update(edited_df)
+            st.success('変更がfinal_dfに保存されました。')
