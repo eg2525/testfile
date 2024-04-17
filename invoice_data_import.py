191a2bd87d234325a68b32d18cabd64c6644536a
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-04-17 18:10:10 +0900

    Add files via upload

--- invoice_data_import.py
+++ invoice_data_import.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import pandas as pd
 
-columns = ["つくば", "羽生", "王子", "三鷹", "仙台", "川口", "船橋", "南森町", "高田馬場", "横浜関内", "福岡天神", "大宮"]
-index = ['後期高齢', '国保窓口入金', '社保窓口入金']
+index = ["つくば", "羽生", "王子", "三鷹", "仙台", "川口", "船橋", "南森町", "高田馬場", "横浜関内", "福岡天神", "大宮"]
+columns = ['後期高齢', '国保窓口入金', '社保窓口入金']
 
 # 空のDataFrameを作成
 df = pd.DataFrame(index=index, columns=columns)
