c9c597b7d6b51be2f7e4b1ec6683ff43133ec124
eg2525 <149653970+eg2525@users.noreply.github.com>
2024-04-18 10:17:23 +0900

    Add files via upload

--- invoice_data_import.py
+++ invoice_data_import.py
@@ -16,19 +16,17 @@ if st.checkbox("OK"):
     df = df.applymap(lambda x: pd.to_numeric(x, errors='ignore'))
     df = df.applymap(lambda x: x * 7 if pd.notna(x) and isinstance(x, (int, float)) else x)
 
-    # '国保窓口入金' と '後期高齢' の金額を合計して新たなカラム '国保' に出力
     df['国保'] = df['国保窓口入金'] + df['後期高齢']
-
-    # 不要になったカラムを削除
     df.drop(['国保窓口入金', '後期高齢'], axis=1, inplace=True)
-
-    # カラム名を変更
     df.rename(columns={'社保窓口入金': '社保'}, inplace=True)
-
-    # 結果のDataFrameを表示
-    st.write(df)
 
     output_columns = ['収支区分', '発生日', '取引先', '税区分', '勘定科目', '品目', '部門', '金額']
     output_df = pd.DataFrame(columns=output_columns)
 
-    
+    # dfの値をoutput_dfに移す
+    for col in df.columns:
+        for idx in df.index:
+            new_row = {'収支区分': '', '発生日': '', '取引先': '', '税区分': '', '勘定科目': '', '品目': col, '部門': idx, '金額': df.at[idx, col]}
+            output_df = output_df.append(new_row, ignore_index=True)
+
+    st.write(output_df)
